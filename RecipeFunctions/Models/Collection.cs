3c9e858aa0824208c8725e57d09ed483999b103e
Tim Freese <freese.tim@gmail.com>
2021-04-16 23:13:50 -0500

    adding collection functions

---
+++ RecipeFunctions/Models/Collection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeFunctions.Models
+{
+	public class Collection
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString("n");
+
+		public string CollectionName { get; set; }
+	}
+}
